dcaa71583e3b68734056125057a21db887526a22
Waly <ahmadouwalyndiaye@gmail.com>
2022-09-26 22:37:28 +0000

    Hide env var

--- nuxt.config.js
+++ nuxt.config.js
@@ -129,7 +129,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://10.42.2.54:8070/api/',
+    baseURL: process.env.BASE_API_URL,
   },
 
   auth: {
