2378b2d41a09be1ac4ec5f69a34b4d2d4bbae85d
Waly <ahmadouwalyndiaye@gmail.com>
2022-09-26 15:29:07 +0000

    Add dashboard page

--- pages/suspects.vue
+++ pages/suspects.vue
@@ -1,128 +1,131 @@
 <template>
-  <!-- Basic table -->
-  <section id="basic-datatable">
-    <div class="row">
-      <div class="col-12">
-        <div class="card">
-          <table class="datatables-basic table">
-            <tbody></tbody>
-            <thead>
-              <tr>
-                <th></th>
-                <th></th>
-                <th>id</th>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Date</th>
-                <th>Salary</th>
-                <th>Status</th>
-                <th>Action</th>
-              </tr>
-            </thead>
-          </table>
+  <no-ssr>
+    <!-- Basic table -->
+    <section id="basic-datatable">
+      <div class="row">
+        <div class="col-12">
+          <div class="card">
+            <table class="datatables-basic table">
+              <tbody></tbody>
+              <thead>
+                <tr>
+                  <th></th>
+                  <th></th>
+                  <th>id</th>
+                  <th>Name</th>
+                  <th>Email</th>
+                  <th>Date</th>
+                  <th>Salary</th>
+                  <th>Status</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+            </table>
+          </div>
         </div>
       </div>
-    </div>
-    <!-- Modal to add new record -->
-    <div id="modals-slide-in" class="modal modal-slide-in fade">
-      <div class="modal-dialog sidebar-sm">
-        <form class="add-new-record modal-content pt-0">
-          <button
-            type="button"
-            class="btn-close"
-            data-bs-dismiss="modal"
-            aria-label="Close"
-          >
-            ×
-          </button>
-          <div class="modal-header mb-1">
-            <h5 id="exampleModalLabel" class="modal-title">New Record</h5>
-          </div>
-          <div class="modal-body flex-grow-1">
-            <div class="mb-1">
-              <label class="form-label" for="basic-icon-default-fullname"
-                >Full Name</label
-              >
-              <input
-                id="basic-icon-default-fullname"
-                type="text"
-                class="form-control dt-full-name"
-                placeholder="John Doe"
-                aria-label="John Doe"
-              />
-            </div>
-            <div class="mb-1">
-              <label class="form-label" for="basic-icon-default-post"
-                >Post</label
-              >
-              <input
-                id="basic-icon-default-post"
-                type="text"
-                class="form-control dt-post"
-                placeholder="Web Developer"
-                aria-label="Web Developer"
-              />
-            </div>
-            <div class="mb-1">
-              <label class="form-label" for="basic-icon-default-email"
-                >Email</label
-              >
-              <input
-                id="basic-icon-default-email"
-                type="text"
-                class="form-control dt-email"
-                placeholder="john.doe@example.com"
-                aria-label="john.doe@example.com"
-              />
-              <small class="form-text">
-                You can use letters, numbers & periods
-              </small>
-            </div>
-            <div class="mb-1">
-              <label class="form-label" for="basic-icon-default-date"
-                >Joining Date</label
-              >
-              <input
-                id="basic-icon-default-date"
-                type="text"
-                class="form-control dt-date"
-                placeholder="MM/DD/YYYY"
-                aria-label="MM/DD/YYYY"
-              />
-            </div>
-            <div class="mb-4">
-              <label class="form-label" for="basic-icon-default-salary"
-                >Salary</label
-              >
-              <input
-                id="basic-icon-default-salary"
-                type="text"
-                class="form-control dt-salary"
-                placeholder="$12000"
-                aria-label="$12000"
-              />
-            </div>
-            <button type="button" class="btn btn-primary data-submit me-1">
-              Submit
-            </button>
+      <!-- Modal to add new record -->
+      <div id="modals-slide-in" class="modal modal-slide-in fade">
+        <div class="modal-dialog sidebar-sm">
+          <form class="add-new-record modal-content pt-0">
             <button
-              type="reset"
-              class="btn btn-outline-secondary"
+              type="button"
+              class="btn-close"
               data-bs-dismiss="modal"
+              aria-label="Close"
             >
-              Cancel
+              ×
             </button>
-          </div>
-        </form>
+            <div class="modal-header mb-1">
+              <h5 id="exampleModalLabel" class="modal-title">New Record</h5>
+            </div>
+            <div class="modal-body flex-grow-1">
+              <div class="mb-1">
+                <label class="form-label" for="basic-icon-default-fullname"
+                  >Full Name</label
+                >
+                <input
+                  id="basic-icon-default-fullname"
+                  type="text"
+                  class="form-control dt-full-name"
+                  placeholder="John Doe"
+                  aria-label="John Doe"
+                />
+              </div>
+              <div class="mb-1">
+                <label class="form-label" for="basic-icon-default-post"
+                  >Post</label
+                >
+                <input
+                  id="basic-icon-default-post"
+                  type="text"
+                  class="form-control dt-post"
+                  placeholder="Web Developer"
+                  aria-label="Web Developer"
+                />
+              </div>
+              <div class="mb-1">
+                <label class="form-label" for="basic-icon-default-email"
+                  >Email</label
+                >
+                <input
+                  id="basic-icon-default-email"
+                  type="text"
+                  class="form-control dt-email"
+                  placeholder="john.doe@example.com"
+                  aria-label="john.doe@example.com"
+                />
+                <small class="form-text">
+                  You can use letters, numbers & periods
+                </small>
+              </div>
+              <div class="mb-1">
+                <label class="form-label" for="basic-icon-default-date"
+                  >Joining Date</label
+                >
+                <input
+                  id="basic-icon-default-date"
+                  type="text"
+                  class="form-control dt-date"
+                  placeholder="MM/DD/YYYY"
+                  aria-label="MM/DD/YYYY"
+                />
+              </div>
+              <div class="mb-4">
+                <label class="form-label" for="basic-icon-default-salary"
+                  >Salary</label
+                >
+                <input
+                  id="basic-icon-default-salary"
+                  type="text"
+                  class="form-control dt-salary"
+                  placeholder="$12000"
+                  aria-label="$12000"
+                />
+              </div>
+              <button type="button" class="btn btn-primary data-submit me-1">
+                Submit
+              </button>
+              <button
+                type="reset"
+                class="btn btn-outline-secondary"
+                data-bs-dismiss="modal"
+              >
+                Cancel
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
-  </section>
-  <!--/ Basic table -->
+    </section>
+    <!--/ Basic table -->
+  </no-ssr>
 </template>
 
 <script>
 export default {
   name: 'ListSuspcets',
   middleware: 'auth',
+  // mounted() {}
 }
 </script>
