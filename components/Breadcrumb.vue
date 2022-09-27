35c7c2bfe595d54b5c9e5a5823e66ed99d179dce
Waly <ahmadouwalyndiaye@gmail.com>
2022-09-27 10:54:12 +0000

    Handle breadcrumbs using config object

--- components/Breadcrumb.vue
+++ components/Breadcrumb.vue
@@ -3,25 +3,50 @@
     <div class="content-header-left col-md-9 col-12 mb-2">
       <div class="row breadcrumbs-top">
         <div class="col-12">
-          <h2 class="content-header-title float-start mb-0">Mise en cause</h2>
+          <h2 class="content-header-title float-start mb-0">
+            {{ breadcrumbs.title }}
+          </h2>
           <div class="breadcrumb-wrapper">
             <ol class="breadcrumb">
-              <li class="breadcrumb-item"><a href="/">Accueil</a></li>
-              <!-- <li class="breadcrumb-item">
-                <a href="#">lis</a>
-              </li> -->
-              <li class="breadcrumb-item active">liste des mises en cause</li>
+              <li
+                v-for="(breadcrumb, i) in breadcrumbs.options"
+                :key="i"
+                :class="[
+                  'breadcrumb-item',
+                  i === Object.keys(breadcrumbs.options).length - 1
+                    ? 'active'
+                    : '',
+                ]"
+              >
+                <a v-if="breadcrumb.url" :href="breadcrumb.url">
+                  {{ breadcrumb.title }}
+                </a>
+                <template v-else> {{ breadcrumb.title }} </template>
+              </li>
             </ol>
           </div>
         </div>
       </div>
     </div>
     <div
+      v-if="actions"
       class="content-header-right text-md-end col-md-3 col-12 d-md-block d-none"
     >
-      <div class="mb-1 breadcrumb-right">
-        <!-- Actions -->
-      </div>
+      <div class="mb-1 breadcrumb-right"></div>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'BreadcrumbsComponent',
+  computed: {
+    breadcrumbs() {
+      return this.$store.state.breadcrumbs
+    },
+    actions() {
+      return this.$store.state.actions
+    },
+  },
+}
+</script>
--- store/index.js
+++ store/index.js
@@ -1,24 +1,22 @@
 export const state = () => ({
-  breacrumbs: [],
+  breadcrumbs: [],
+  actions: [],
 })
 
 export const getters = {
   getBreadcrumbs(state) {
-    return state.breacrumbs
+    return state.breadcrumbs
+  },
+  getActions(state) {
+    return state.actions
   },
 }
 
 export const mutations = {
   setBreadcrumbs(state, value) {
-    state.breacrumbs = value
+    state.breadcrumbs = value
   },
-}
-
-export const actions = {
-  fetchCounter({ state }) {
-    // make request
-    const res = { data: 10 }
-    state.counter = res.data
-    return res.data
+  setActions(state, value) {
+    state.actions = value
   },
 }
